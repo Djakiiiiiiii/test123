a86aa4bc594af56d19954df430b79fd45ccc9be4
Djekk <syamiladzdzaki08@gmail.com>
2026-07-01 22:49:48 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
         "use": "sig",
         "kid": "2cbc0ff1-62ba-4b31-af99-1e5a064c5ddd",
         "alg": "RS256",
-        "n": "2yClzeAQsJ92igvwwQH0r8UxXUd45IX4sbbLKF9bmTeW7iKXdV16vM8wJFEkuP9XS4GOj4CBJFiD4EMYvxfa9DL4AMeS3XWkwvVBoPl8-zC40Gk-MmafbE9xikLgptHxZzxQAZe6jyUjqa0hxx--lTkS2T0ewhNGNdzubPnego0OppjVZCCjrxyqYWX0onWTOFUdZ6g-Sx7oL21BA8znDh7BDU8hPnoV2PP6LWir8x_1CNCjftjXXrA5V-PN1R0zZ3ouBnEjilLSsmhHc2TbhJl3OYAxZNk7t2O_bvBuiA7niNBnGL-sy_ZO9f2hYFsb7Ivyy3Xgb0FjqUq9UrBwHQ",
+        "n": "XXXXX...",  // GANTI DENGAN YANG BARU DARI TERMINAL
         "e": "AQAB"
       }
     ]
